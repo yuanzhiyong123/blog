f2972f2b627e22b7a5d89ed74fe2b02ef662a645
yuanzhiyong <1060915552@qq.com>
2018-08-29 20:53:50 +0800

    fix ;

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,8 @@
 </template>
 
 <script>
-import Header from 'components/header/Header';
-import Footer from 'components/footer/Footer';
+import Header from 'components/header/Header'
+import Footer from 'components/footer/Footer'
 export default {
   name: 'App',
   components: {
